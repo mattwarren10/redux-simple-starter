b8cfd1e431c2388882b761e0c3e9a6e398240d1e
Matt Warren <mwarren@harding.edu>
2017-08-17 15:22:04 -0400

    add youtube-api-search pkg. add search bar component to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCCnPbp9INLQUDMwDIqZsnNjXdO0RUyma0'
+
 // Create a new compnent. Should produce HTML
 
 const App = () => {
-	return <div>Hi</div>;
-}
+	return (
+	<div>
+	  <SearchBar />
+	</div>
+	);
+};
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
