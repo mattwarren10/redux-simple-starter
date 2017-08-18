7d1a796e57df3cd0d7b543918429f3c83d7cb311
Matt Warren <mwarren@harding.edu>
2017-08-18 13:44:13 -0400

    fix bug that prevented video from being displayed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SearchBar extends Component {
   constructor(props) {
-  	super(props);
+    super(props);
 
-  	this.state = { term: '' };
+    this.state = { term: "" };
   }
+
   render() {
-    return( 
-    <div className="search-bar"> 
-      <input 
-        value={this.state.term}
-        onChange={event => this.onInputChange(event.target.value)} 
-      /> 
-    </div>
-    )
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
   }
 
   onInputChange(term) {
-		this.setState({term});
-		this.props.onSearchTermChange(term);
-  };
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
 }
-
 
 export default SearchBar;
