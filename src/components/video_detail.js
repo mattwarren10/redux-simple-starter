837666dd0dbd593f86f7bdf3338e03da58f8598b
Matt Warren <mwarren@harding.edu>
2017-08-18 13:10:26 -0400

    allow VideoDetail to change when a user clicks a video from VideoList

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const VideoDetail = ({video}) => {
+	if (!video) {
+		return (<div>Loading...</div>);
+	}
+
 	const videoId = video.id.videoId
-	const url = `https://www.youtube.com/embed/${video.id}`
+	const url = `https://www.youtube.com/embed/${video.id}`;
+	console.log({videoId})
   return (
   	<div className="video-detail col-md-8">
   	  <div className="embed-responsive embed-responsive-16by9">
-  	  	<iframe src="{url}" frameborder="0" className="embed-responsive-item"></iframe>
+  	  	<iframe src="{url}" className="embed-responsive-item"></iframe>
   	  </div>
   	  <div className="details">
   	  	<div>{video.snippet.title}</div>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
-const API_KEY = 'AIzaSyCCnPbp9INLQUDMwDIqZsnNjXdO0RUyma0'
+const YOUTUBE_API_KEY = 'AIzaSyCCnPbp9INLQUDMwDIqZsnNjXdO0RUyma0'
+const GOOGLE_MAPS_API_KEY = 'AIzaSyDdcFGPEu80h_fp_EdE3CYnp6MQFI9MJdU'
 
 
 class App extends Component {
@@ -13,22 +15,32 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] }
+    this.state = { 
+    	videos: [],
+    	selectedVideo: null
 
-	YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
-  	  this.setState({ videos }) // shorter syntax
-  	  // this.setState({ videos: videos}) <= reality
+    };
+
+		YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+  	  this.setState({ 
+  	  	videos: videos, 
+				selectedVideo: videos[0]
+  	  })
 	});
 
   }
 
   render() {
-	return (
-	  <div>
-	    <SearchBar />
-	    <VideoList videos={this.state.videos} />
-	  </div>
-	);	
+		return (
+		  <div>
+		    <SearchBar />
+		    <VideoDetail video={this.state.selectedVideo}/>
+		    <VideoList 
+		    	onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+		    	videos={this.state.videos} 
+		    />
+		  </div>
+		);	
   }
 };
 
